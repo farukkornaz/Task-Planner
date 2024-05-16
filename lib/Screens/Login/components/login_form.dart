e6ace75440b7ca04cce5f84489cbc51056f22092
faruk kornaz <fkornaz@hotmail.com>
2024-05-16 16:28:55 +0300

    init

---
+++ lib/Screens/Login/components/login_form.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../../../components/already_have_an_account_acheck.dart';
+import '../../../constants.dart';
+import '../../Signup/signup_screen.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController emailTextController = TextEditingController();
+    final TextEditingController passwordTextController = TextEditingController();
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            controller: emailTextController,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "e-mail adresiniz",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              controller: passwordTextController,
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: const InputDecoration(
+                hintText: "şifreniz",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          ElevatedButton(
+            onPressed: () {
+              print(emailTextController.value.text);
+              print(passwordTextController.value.text);
+            },
+            child: Text(
+              "Giriş Yap".toUpperCase(),
+            ),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SignUpScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(206, 17, 59, 0.8);
+const kPrimaryLightColor = Color.fromRGBO(206, 17, 59, 0.30);
+
+const double defaultPadding = 15.0;
